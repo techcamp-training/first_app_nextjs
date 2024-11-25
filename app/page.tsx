5f29e25d7a4265e561d6e83ccb762cb2e8d047a4
sh10n7 <shion.yamauchi@di-v.co.jp>
2024-11-25 16:43:09 +0900

    一覧表示コンポーネント作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,56 @@
 // import styles from "./page.module.css";
-import { Form } from "@/app/_components/Form"
+"use client"
 
-export default function Home() {
+import { Form } from "@/app/_components/Form"
+import React, { useEffect, useState } from "react";
+
+interface Post {
+  id: string;
+  content: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const Home: React.FC = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+  
+  useEffect(()=> {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch("/api/posts", {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        const { posts }: { posts: Post[] }= await response.json();
+        setPosts(posts)
+
+      } catch (error){
+        console.log("APIリクエストエラー", error);
+        throw new Error("投稿の取得ができませんでした");
+      }
+    };
+    fetchPosts();
+  },[]);
+
+
   return (
     <>
       <h2>トップページ</h2>
       <Form />
+      <div>
+        <ul>
+          {posts.map((post) => {
+            return(
+              <li key={post.id}>
+                <p>{post.content}</p>
+                <p>{post.createdAt.toString()}</p>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
     </>
   );
 }
+export default Home;
