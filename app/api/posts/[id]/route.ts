4a6443337b84af1df187bb94451ccfa9d5142d0a
sh10n7 <shion.yamauchi@di-v.co.jp>
2024-11-25 18:10:38 +0900

    投稿更新のAPI作成

---
+++ app/api/posts/[id]/route.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export const PUT = async (request: NextRequest, { params }: { params: { id: string } }) => {
+  const { id } =  await params;
+  const postId = parseInt(id, 10);
+  const { content } = await request.json();
+
+  try {
+    const updatedPost = await prisma.post.update({
+      where: {
+        id: postId
+      },
+      data: {
+        content,
+      },
+    })
+
+    return NextResponse.json({ response: "ok", post: updatedPost }, { status: 200} );
+  } catch(error) {
+    return NextResponse.json({ response: "error", message: "更新に失敗しました"}, { status: 500});
+  }
+}
