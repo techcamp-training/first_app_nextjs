d30396ba52b642c9c15e4a4b758c53bc74b30c8e
sh10n7 <shion.yamauchi@di-v.co.jp>
2024-11-25 16:42:46 +0900

    一覧表示のAPI作成

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -5,7 +5,6 @@ const prisma = new PrismaClient();
 
 export const POST = async (request: NextRequest)=> {
   const { content } = await request.json();
-  console.log(content)
 
   try {
     const post = await prisma.post.create({
@@ -17,5 +16,14 @@ export const POST = async (request: NextRequest)=> {
     return NextResponse.json({ response: "ok", post: post}, { status: 200} )
   } catch(error) {
     return NextResponse.json({ response: "error", message: "保存に失敗しました"}, { status: 500 });
+  }
+}
+
+export const GET = async () => {
+  try {
+    const allPosts = await prisma.post.findMany();
+    return NextResponse.json({ response: "ok", posts: allPosts}, { status: 200} )
+  } catch (error) {
+    return NextResponse.json({ status: "error", message: "データ取得に失敗しました"}, { status: 500 });
   }
 }
