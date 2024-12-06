a4a13a0ee89d914dee8b74f72fec435aa34e4147
sh10n7 <shion.yamauchi@di-v.co.jp>
2024-12-06 16:45:03 +0900

    トップページのrエラーハンドリング

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import { Post } from "./_components/Post";
 const Home: React.FC = () => {
   const [posts, setPosts] = useState<ResponsePost[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
 
   // 一覧表示データの更新
   const addPost = (newPost: ResponsePost) => {
@@ -35,37 +36,44 @@ const Home: React.FC = () => {
             "Content-Type": "application/json",
           },
         });
+
+        // ステータスが200以外の場合はエラーとして処理
+        if (!response.ok) {
+          throw new Error("投稿の取得に失敗しました");
+        }
+
         const { posts }: { posts: ResponsePost[] }= await response.json();
         setPosts(posts)
-        setIsLoading(false)
       } catch (error){
         console.log("APIリクエストエラー", error);
-        throw new Error("投稿の取得ができませんでした");
+        setError("投稿の取得ができませんでした");
+      } finally {
+        setIsLoading(false);
       }
     };
     fetchPosts();
   },[]);
-
 
   return (
     <>
       <h2>トップページ</h2>
       <Form addPost={addPost}/>
       <div>
-        {isLoading ? (
-          <p>読み込み中...</p>
-        ) : (
+        {isLoading ? ( <p>読み込み中...</p>) : error ? (<p>投稿を取得できませんでした</p>) : 
+        (
           <ul>
-            {posts.map((post) => {
-              return(
+            {posts && posts.length > 0 ? (
+              posts.map((post) => (
                 <Post 
                   key={post.id}
                   post={post}
                   updatePost={updatePost}
                   deletedPost={deletePost}
                 />
-              )
-            })}
+              ))
+            ) : (
+              <p>投稿がありません</p>
+            )}
           </ul>
         )}
       </div>
